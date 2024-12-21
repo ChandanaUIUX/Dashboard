b196165155c235e0877060043eb560c34629640c
ChandanaUIUX <chandana.uiuxdesigner@gmail.com>
2024-12-21 16:20:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Component/Headerpage/Header';
+import Sidebar from './Component/Sidebarpage/Sidebar'
+import Home from './Component/Homepage/Home';
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
+}
+
+export default App
